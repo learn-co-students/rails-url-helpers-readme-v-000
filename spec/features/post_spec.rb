339af88c4d71802e754284dd568ab132e6bb78dd
Andrew Papacostas <a_papacostas@yahoo.com>
2016-07-21 06:38:28 +0000

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -28,3 +28,11 @@ describe 'index page' do
     expect(page).to have_link(second_post.title, href: post_path(second_post))
   end
 end
+
+describe 'index page' do
+  it 'links to post page' do
+    second_post = Post.create(title: "My Title", description: "My post description")
+    visit posts_path
+    expect(page).to have_link(second_post.title, href: post_path(second_post))
+  end
+end
