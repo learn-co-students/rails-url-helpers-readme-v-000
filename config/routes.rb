944b420ec84fffdeb405be02eac7595bcb8367fd
github username <github email address>
2017-10-31 16:15:26 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
+  get '/register', to: 'users#new', as: 'register'
 end
+# On line 3: route is GET request to localhost:3000/register
+# This route is mapped to the #new controller action (method) in the UsersController class
+# as: 'register' changes the name of the route (= prefix of route helper method) to register, so
+# I can use the #register_path route helper method
+# to return the string URL (href attribute value) of
+# the link to the page presenting form to create a new post
