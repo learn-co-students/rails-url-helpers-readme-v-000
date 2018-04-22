d29bcbadd81488803e75c62240c824571fd238c4
Aly Alidina <aly_alidina@hotmail.com>
2018-04-22 04:30:10 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
+#To customis the path use as:. In this case the user will see register url and
+#it can be utilized in the app
+  get '/register', to: 'users#new', as: 'register'
 end
