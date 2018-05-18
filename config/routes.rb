19f83b29b5260b5e4630fe232d9ec9acc9483c6f
AlexFriedland <friedlandaj@gmail.com>
2018-05-18 10:23:07 +0200

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   #posts routes with index and show pages
   resources :posts, only: [:index, :show]
+
+  #create register page for signups
+  get '/register', to: 'users#new', as: 'register'
 end
