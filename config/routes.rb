e39cd6919cc79d62d132f9182c689261382b188b
Raiet Intraub <raietsaeed@gmail.com>
2018-05-25 19:09:40 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
+
+  get '/register', to: 'users#new', as: 'register'
+  # NOTE : Using the above can now allow for using "register_path" route helper
+
 end
