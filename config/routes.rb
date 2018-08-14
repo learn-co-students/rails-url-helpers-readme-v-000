1c5f5830a9224a633142857c98fc763aa46c546a
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-14 03:15:02 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+
 Rails.application.routes.draw do
+  
   resources :posts, only: [:index, :show]
+  
+  get '/register', to: 'users#new', as: 'register' # written as register_path elsewhere
+  
 end
