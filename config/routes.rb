b84f3c2a75be74dd7edb929decccc13a48ac46de
essbee808 <sheena.myb@gmail.com>
2021-06-04 14:07:48 -0700

    add seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show] #route call
-  get '/register', to: 'users#new', as: 'register'
+  get '/users/new', to: 'users#new', as: 'register'
 end
